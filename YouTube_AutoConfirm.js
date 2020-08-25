f7a1136ca9324778f44305250939f1c31c4568e8
RustyPrime <39623706+RustyPrime@users.noreply.github.com>
2020-08-25 16:30:07 +0200

    fixed bug that caused confirming too often

--- YouTube_AutoConfirm.js
+++ YouTube_AutoConfirm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube_AutoConfirm
 // @namespace    https://waryor.com/
-// @version      0.1.2
+// @version      0.1.3
 // @description  Auto-Clicks on "Yes" when asked "Video paused. Continue watching?"
 // @author       RustyPrimeLUX
 // @match        https://www.youtube.com/watch?v=*
@@ -13,15 +13,33 @@
 (function() {
     var checkDelay = 2000; // checks every 2 seconds if the pop-up has appeared
 
+
+    var popUp = null;
     'use strict';
     setInterval(function(){
-        var popUp = document.querySelector("yt-confirm-dialog-renderer");
-        if(popUp !== undefined && popUp !== null){
-            var button = popUp.querySelector("paper-button#button yt-formatted-string#text");
-            if(button.textContent === "Yes"){
-                console.log("clicking on yes");
-                button.click();
+        if(popUp === null){
+            popUp = document.querySelector("paper-dialog.ytd-popup-container");
+        }
+        if(popUp !== undefined){
+            if(popUp !== null){
+                if(popUp.hasAttribute("aria-hidden")){
+                    if(popUp.getAttribute("aria-hidden") !== true && popUp.getAttribute("aria-hidden") !== "true"){
+                        FindAndClickButton(popUp);
+                    }
+                }
+                else{
+                    FindAndClickButton(popUp);
+                }
             }
         }
     }, checkDelay);
+
+    function FindAndClickButton(popUp){
+        var button = popUp.querySelector("paper-button#button yt-formatted-string#text");
+        if(button.textContent === "Yes"){
+            console.log("clicking on yes");
+            button.click();
+        }
+    }
 })();
+
