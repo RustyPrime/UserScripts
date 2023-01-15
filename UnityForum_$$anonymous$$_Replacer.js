50d037ef3d42e91966639230b03e09b12ff8bf93
RustyPrime <39623706+RustyPrime@users.noreply.github.com>
2023-01-15 15:51:46 +0100

    added license

--- UnityForum_$$anonymous$$_Replacer.js
+++ UnityForum_$$anonymous$$_Replacer.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=unity.com
 // @downloadURL  https://raw.githubusercontent.com/RustyPrime/UserScripts/master/UnityForum_$$anonymous$$_Replacer.js
 // @updateURL    https://raw.githubusercontent.com/RustyPrime/UserScripts/master/UnityForum_$$anonymous$$_Replacer.js
+// @license MIT
 // @grant        none
 // ==/UserScript==
 
