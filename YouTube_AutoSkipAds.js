49ff2ebc0293ba023d8c958189b9ae53cb163fc6
RustyPrime <39623706+RustyPrime@users.noreply.github.com>
2024-08-09 22:38:03 +0200

    Update YouTube_AutoSkipAds.js
    added redirect to current timestamp when error screen is shown

--- YouTube_AutoSkipAds.js
+++ YouTube_AutoSkipAds.js
@@ -1,44 +1,88 @@
 // ==UserScript==
 // @name         YouTube Auto Skip Ads
 // @namespace    none
-// @version      0.5
+// @version      0.6
 // @description  autoclicks on youtube's skip ad buttons once they appear, also auto-closes banner ads
 // @author       RustyPrimeLUX
 // @match        https://www.youtube.com/*
-// @downloadURL  https://raw.githubusercontent.com/RustyPrime/UserScripts/master/YouTube_AutoSkipAds.js
-// @updateURL    https://raw.githubusercontent.com/RustyPrime/UserScripts/master/YouTube_AutoSkipAds.js
 // @license      MIT
 // @grant        none
+// @downloadURL  https://update.greasyfork.org/scripts/412698/YouTube%20Auto%20Skip%20Ads.user.js
+// @updateURL 	 https://update.greasyfork.org/scripts/412698/YouTube%20Auto%20Skip%20Ads.meta.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-	var debug = false;
-    setInterval(function(){
+    var currentVideoTime = 0;
 
-        var skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button");
-        if(skipAdBtn != null && isVisible(skipAdBtn)){
-            if(debug) console.log("clicking on skip ads");
+    var videoPlayer = document.querySelector(".video-stream.html5-main-video");
+    var skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button");
+    var closeBannerBtn = document.querySelector("button.ytp-ad-overlay-close-button");
+    var errorScreen = document.querySelector("#error-screen");
+
+    setInterval(function(){
+        if (getVideoPlayer() && isVisible(videoPlayer)){
+            var currentTime = Math.floor(videoPlayer.currentTime);
+            if (currentTime > 30)
+            {
+                currentVideoTime = currentTime;
+            }
+        }
+
+        if(getSkipAdsButton() && isVisible(skipAdBtn)){
             skipAdBtn.click();
         }
-        else{
-            if(debug) console.log("found no ads: skipAdBtn=" + skipAdBtn);
-        }
 
-        var closeBannerBtn = document.querySelector("button.ytp-ad-overlay-close-button");
-        if(closeBannerBtn != null && isVisible(closeBannerBtn)){
-            if(debug) console.log("clicking on close banner ad");
+        if(getCloseBannerButton() && isVisible(closeBannerBtn)){
             closeBannerBtn.click();
         }
-        else{
-            if(debug) console.log("found no banner ads: skipBannerBtn=" + closeBannerBtn);
-        }
 
-	var errorScreen = document.querySelector("#error-screen");
-        if (errorScreen != null && isVisible(errorScreen)){
-            window.location.reload();
+        if (getErrorScreen() && isVisible(errorScreen)){
+            if (currentVideoTime == 0)
+            {
+                window.location.reload();
+            }
+            else{
+                var currentLink = window.location.href;
+                var playAtTimestampQuery = "&t=";
+
+                if (currentLink.includes(playAtTimestampQuery)){
+                    currentLink = currentLink.split(playAtTimestampQuery)[0];
+                }
+
+                var linkWithTimestamp = currentLink + playAtTimestampQuery + currentVideoTime;
+                window.location.href = linkWithTimestamp;
+            }
         }
     }, 1000);
+
+    function getVideoPlayer(){
+        if (videoPlayer == null){
+            videoPlayer = document.querySelector(".video-stream.html5-main-video");
+        }
+        return videoPlayer != null;
+    }
+
+    function getSkipAdsButton() {
+        if (skipAdBtn == null){
+            skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button");
+        }
+        return skipAdBtn != null;
+    }
+
+    function getCloseBannerButton() {
+        if (closeBannerBtn == null) {
+            closeBannerBtn = document.querySelector("button.ytp-ad-overlay-close-button");
+        }
+        return closeBannerBtn != null;
+    }
+
+    function getErrorScreen(){
+        if (errorScreen == null){
+            errorScreen = document.querySelector("#error-screen");
+        }
+        return errorScreen != null;
+    }
 
     function isVisible(el) {
         return (el.offsetParent !== null)
