a1cfc678fd836bf4294ca652768a1b25a5743389
RustyPrime <39623706+RustyPrime@users.noreply.github.com>
2024-08-19 14:10:13 +0200

    Update YouTube_AutoConfirm.js

--- YouTube_AutoConfirm.js
+++ YouTube_AutoConfirm.js
@@ -1,39 +1,37 @@
 // ==UserScript==
 // @name         YouTube Auto Confirm
-// @namespace    https://waryor.com/
-// @version      0.1.3
+// @version      0.2
 // @description  Auto-Clicks on "Yes" when asked "Video paused. Continue watching?"
 // @author       RustyPrimeLUX
 // @match        https://www.youtube.com/watch?v=*
 // @grant        none
+// @license      MIT
+// @downloadURL  https://update.greasyfork.org/scripts/409090/YouTube%20Auto%20Confirm.user.js
+// @updateURL    https://update.greasyfork.org/scripts/409090/YouTube%20Auto%20Confirm.meta.js
 // ==/UserScript==
 
 (function() {
     var checkDelay = 2000; // checks every 2 seconds if the pop-up has appeared
 
-
     var popUp = null;
     'use strict';
     setInterval(function(){
-        if(popUp === null){
-            popUp = document.querySelector("yt-button-renderer#confirm-button");
+        if(popUp == null){
+            popUp = document.querySelector("#confirm-button");
         }
-        if(popUp !== undefined){
-            if(popUp !== null){
-                if(popUp.hasAttribute("aria-hidden")){
-                    if(popUp.getAttribute("aria-hidden") !== true && popUp.getAttribute("aria-hidden") !== "true"){
-                        FindAndClickButton(popUp);
-                    }
-                }
-                else{
-                    FindAndClickButton(popUp);
-                }
-            }
+        if(popUp == null)
+        {
+            return;
         }
+
+        if (isVisible(popUp))
+        {
+            popUp.click();
+        }
+        
     }, checkDelay);
 
-    function FindAndClickButton(popUp){
-        var button = popUp.querySelector("a");
-        if(button !== undefined) button.click();
+    function isVisible(el) {
+        return (el.offsetParent !== null)
     }
 })();
