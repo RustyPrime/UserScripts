aa4bb266f509aac5468147468c3f3b5b02981037
RustyPrime <39623706+RustyPrime@users.noreply.github.com>
2024-08-23 18:43:11 +0200

    Update YouTube_AutoSkipAds.js
    fixed skip button not being found anymore

--- YouTube_AutoSkipAds.js
+++ YouTube_AutoSkipAds.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         YouTube Auto Skip Ads
-// @namespace    none
-// @version      0.8
+// @version      0.9
 // @description  autoclicks on youtube's skip ad buttons once they appear, also auto-closes banner ads
 // @author       RustyPrimeLUX
 // @match        https://www.youtube.com/*
 // @license      MIT
+// @namespace    none
 // @grant        none
 // @downloadURL  https://update.greasyfork.org/scripts/412698/YouTube%20Auto%20Skip%20Ads.user.js
 // @updateURL 	 https://update.greasyfork.org/scripts/412698/YouTube%20Auto%20Skip%20Ads.meta.js
@@ -16,7 +16,7 @@
     var currentVideoTime = 0;
 
     var videoPlayer = document.querySelector(".video-stream.html5-main-video");
-    var skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button") || document.querySelector(".ytp-skip-ad-button__text");
+    var skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button") || document.querySelector(".ytp-skip-ad-button__text") || document.querySelector(".ytp-ad-skip-button-text-centered");
     var closeBannerBtn = document.querySelector("button.ytp-ad-overlay-close-button");
     var errorScreen = document.querySelector("#error-screen");
 
@@ -65,7 +65,7 @@
 
     function getSkipAdsButton() {
         if (skipAdBtn == null || (skipAdBtn != null && !document.body.contains(skipAdBtn))){
-            skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button") || document.querySelector(".ytp-skip-ad-button__text");
+            skipAdBtn = document.querySelector("div.ytp-ad-text.ytp-ad-skip-button-text") || document.querySelector(".ytp-skip-ad-button") || document.querySelector(".ytp-skip-ad-button__text") || document.querySelector(".ytp-ad-skip-button-text-centered");
         }
         return skipAdBtn != null;
     }
